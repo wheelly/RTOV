c23fce5619d299543c766c648aad147c4f95b34f
wheelly <borisk@kimaia.com>
2020-05-03 12:55:03 +0300

    Default properties

--- src/lib/prop-handlers.ts
+++ src/lib/prop-handlers.ts
@@ -8,7 +8,7 @@ export const getPropName = (name: string) => PROP_PREFIX + name;
 
 export const setReadOnlyProperty = (obj: any, prop: string, data: any) => {
   Object.defineProperty(obj, getPropName(prop), {
-    value: data,
+    value: data || obj[prop], //default value
     writable: false,
     configurable: true //let it be redefined
   });
